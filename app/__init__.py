987ca073dc4de222bbfefe28f3c3158c0c70f946
Fangmbeng <yorobeau@gmail.com>
2023-05-30 12:57:05 -0600

    'Fangmbeng'

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,11 @@ from flask import Flask
 from flask_migrate import Migrate
 # Import the Config class from the config module - will have all of the app's configurations
 from config import Config
-from app.api.routes import api
-from app.authentication.routes import auth
-from app.site.routes import site
+from app.blueprints.api.routes import api
+from app.blueprints.authentication.routes import auth
+from app.blueprints.site.routes import site
 from app.models import db as root_db, login, ma
+from flask_cors import CORS
 # Create an instance of the Flask Class called app
 app = Flask(__name__)
 app.register_blueprint(site)
@@ -16,7 +17,7 @@ app.register_blueprint(api)
 # Configure the app using the Config class
 app.config.from_object(Config)
 app.config['SECRET_KEY'] = " "
-
+CORS(app)
 # Create an instance of SQLAlchemy to represent our database
 root_db.init_app(app)
 # Create an instance of Migrate to represent our migration engine
